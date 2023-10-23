17f5f87da2b605e885884c4f8c7592d5426f35c2
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-10-23 19:54:07 -0300

    fixed

--- src/coordenadores.js
+++ src/coordenadores.js
@@ -1,11 +1,11 @@
-const url = "https://951p4oju.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22coordinatorCard%22%5D%7B%0A++%22id%22%3A+_id%2C%0A++%22name%22%3A+name%2C%0A++%22gender%22%3A+gender%2C%0A++%22img%22%3A+coordinatorImg.asset-%3Eurl%0A%7D";
+const urlCoordenador= "https://951p4oju.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22coordinatorCard%22%5D%7B%0A++%22id%22%3A+_id%2C%0A++%22name%22%3A+name%2C%0A++%22gender%22%3A+gender%2C%0A++%22img%22%3A+coordinatorImg.asset-%3Eurl%0A%7D";
 
 async function getCoordenadores() {
     const nome = document.querySelector(".name-coordenador");
     const imagem = document.querySelector(".img-coordenador");
     const genero = document.querySelector(".genero-coordenador");
 
-    const response = await fetch(url);
+    const response = await fetch(urlCoordenador);
     const { result: coordenador } = await response.json();
 
     nome.innerHTML = coordenador[0].name;
--- src/instagramPosts.js
+++ src/instagramPosts.js
@@ -1,10 +1,10 @@
-const url = "https://951p4oju.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22instagramPost%22%5D%7B%0A++%22id%22%3A+_id%2C%0A++%22img%22%3A+title.asset-%3Eurl%2C%0A++%22postLink%22%3A+postLink%0A%7D";
+const urlPosts = "https://951p4oju.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22instagramPost%22%5D%7B%0A++%22id%22%3A+_id%2C%0A++%22img%22%3A+title.asset-%3Eurl%2C%0A++%22postLink%22%3A+postLink%0A%7D";
 
 async function getPosts() {
     const images = document.querySelector(".redes-img");
-    const response = await fetch(url);
+    const response = await fetch(urlPosts);
     const { result: posts } = await response.json();
-    console.log(posts)
+    console.dir(posts)
 
     posts.forEach(post => {
         const image = document.createElement("img");
